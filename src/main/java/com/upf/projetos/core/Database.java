a7abe21e2a3525ef8477ee5d6ce46295fffc2b6f
williamsimionatto <62775299+williamsimionatto@users.noreply.github.com>
2021-06-14 18:15:30 -0300

    Update Database.java

--- src/main/java/com/upf/projetos/core/Database.java
+++ src/main/java/com/upf/projetos/core/Database.java
@@ -12,8 +12,9 @@ public class Database {
     // private static final String IP = "177.67.253.61";
     private static final String DATABASE = "178344";
     // private static final int PORT = 27017;
+    private static final String URI = "mongodb://178344:178344@177.67.253.61/?authSource=178344&ssl=false";
 
-    private static MongoClientURI uri = new MongoClientURI("mongodb://178344:178344@177.67.253.61/?authSource=178344&ssl=false");
+    private static MongoClientURI uri = new MongoClientURI(URI);
     private static MongoClient mongoClient = new MongoClient(uri);
     private static Morphia morphia = new Morphia();
     private static Datastore datastore = morphia.createDatastore(mongoClient, DATABASE);
